1d3b8c3ac27df1ace0c34e7abfb0b126c435406a
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-03-28 18:43:49 +0300

    Add  else if on my control statement

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function scanInputs() {
+function myFunction() {
 	var A = parseInt(document.scanInput.sideA.value);//converts input for side A to integer
 	var B = parseInt(document.scanInput.sideB.value);//converts for side B
 	var C = parseInt(document.scanInput.sideC.value);//converts for side C
@@ -13,27 +13,26 @@ function scanInputs() {
   var B2C2 = sideBsqrd + sideCsqrd;
   var text;
 	if ((AB > C) && (AC > B) && (BC > A)) {
-    text = "These numbers could form a triangle.";
+    if ((sideAsqrd == B2C2) || (sideBsqrd == A2C2) || (sideCsqrd == A2B2)) 
+    {
+      text = "These numbers could also form a right triangle.";
+    }
+    else if((A === B)&&(A === C)&&(C === B))
+    {
+      text = "This is an EQUILATERAL TRIANGLE"
+    }
+    else if((A !== B)&&(A !== C)&&(C !== B))
+    {
+      text = "This is a SCALENE";
+    }
+    else if(((A === B)&&(A !== C)&&(B !== C)) ||((B === C)&&(A !==B)&&(A !== C ) )||((A === C)&&(B !== C)&&(B !== A)))
+    {
+      text = "This is a ISOCSELES";
+    }
 	}
-	if ((sideAsqrd == B2C2) || (sideBsqrd == A2C2) || (sideCsqrd == A2B2)) 
-  {
-		text = "These numbers could also form a right triangle.";
-	}
-  if((A === B)&&(A === C)&&(C === B))
-  {
-    text= "This is an EQUILATERAL TRIANGLE";
-  }
-  if((A !== B)&&(A !== C)&&(C !== B))
-  {
-    text = "This is a SCALENE";
-  }
-  if((A === B !== C)||(A === C !==B)||(C === B !== C))
-  {
-    text = "This is a ISOCSELES";
-  }
 	else 
   {
 		text = "These 3 sides can not form a triangle.";
   }	
-  document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = text;
 }
