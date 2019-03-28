ae3c78d8cfbc8fcb42e3e32a6cdd3a84d131993d
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-03-28 13:24:24 +0300

    Declares the Three sides of a Triangle

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 function scanInputs(){
-  
+sideA = parseInt(document.scanned_Inputs.sideA.value);
+sideB= parseInt(document.scanned_Inputs.sideB.value);
+sideC = parseInt(document.scanned_Inputs.sideC.value);
+    if (sideA == sideB && sideB == sideC) {
+    alert("Equalateral");
+    }
+    if (sideA == sideB && != sideC) {
+    alert("Isosceles");
+    }
+    if (sideA != sideB == sideC) {
+    alert("Isosceles");
+    }
+    if (sideA != sideB != sideC != sideA) {
+    alert("Scalene!");
+    }
 }
